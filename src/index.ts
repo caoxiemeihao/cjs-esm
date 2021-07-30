9af1f8b65e1ca8034397433f2eb43d6c7d4640e2
草鞋没号 <308487730@qq.com>
2021-07-30 15:03:07 +0800

    faet: add index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,38 @@
+import { createContext, CreateContextOptions } from './context'
+import { createAnalyzer } from './analyzer'
+import { createWriter } from './writer'
 
-console.log(process.env)
+function transform(options: CreateContextOptions) {
+  const context = createContext(options)
+  const analyzer = createAnalyzer(context)
 
-process.exit()
+  try {
+    analyzer.analyze()
+  } catch (error) {
+    if (error.pos === null && context.node) {
+      error.pos = context.node.start
+    }
+    throw error
+  }
+  if (
+    !context.moduleNodes.length &&
+    !context.requireNodes.length &&
+    !context.exportsNodes.length
+  ) {
+    return Promise.resolve({
+      code: context.code,
+      map: null,
+      isTouched: false,
+    })
+  }
+
+  return createWriter(context).write()
+    .then(() => ({
+      code: context.s.toString(),
+      map: options.sourceMap ? context.s.generateMap({ hires: true }) : null,
+      isTouched: true,
+      context,
+    }))
+}
+
+export { transform }
