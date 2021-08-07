c536092d08a96e273803909a447ea590b294e8b9
草鞋没号 <308487730@qq.com>
2021-08-07 17:39:50 +0800

    chore: UPDATE

---
+++ .tmp/commonjs/require.js
@@ -0,0 +1,18 @@
+const parse = require('acorn').parse;
+const { ancestor, simple } = require('acorn-walk')
+const acorn = require('acorn');
+const home = require('@/views/home').home
+
+require('vite')
+
+const dict = {
+  news: require('@/views/news').default,
+  home: require('@/views/home'),
+}
+
+const arr = [
+  require('@/views/news').news,
+  require('@/views/home'),
+]
+
+module.exports = { dict, arr };
