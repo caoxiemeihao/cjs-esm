9fc84a780f3a2f5831d252a398064fadabd9532a
草鞋没号 <308487730@qq.com>
2021-08-08 10:28:14 +0800

    cjs-esm: transformImport@beta-2

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import { Context, createContext } from './context'
 import { createAnalyze } from './analyze'
-
+import {
+  createTransform,
+  TransformExporttOptions,
+  TransformImportOptions,
+} from './transform'
 
 export interface TransformeOptions {
   /**
    * @default false
    */
   sourcemap?: boolean
+  transformImport?: TransformImportOptions
+  transformExport?: TransformExporttOptions
 }
 
 export interface Transformed {
@@ -15,19 +21,23 @@ export interface Transformed {
   context: Context
 }
 
-export function transform(code: string, options?: TransformeOptions): Transformed {
+export function transform(code: string, options: TransformeOptions = {}): Transformed {
   const context = createContext({ code })
   const analyze = createAnalyze(context)
 
   try {
     analyze.analyze()
+    createTransform(context, {
+      transformImport: options.transformImport,
+      transformExport: options.transformExport,
+    }).transform()
   } catch (error) {
     throw error
   }
 
   return {
-    code,
-    sourcemap: null,
+    code: context.transformedCode,
+    sourcemap: context.sourcemap,
     context,
   }
 }
